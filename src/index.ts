deaccbc76de31521478d5b1e6acb1ffe2be10a8e
Prince Appiah <pappiah00@gmail.com>
2022-10-31 22:17:29 +0000

    Added cors and helmet

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express, { Application, Express, Request, Response } from "express";
+import helmet from "helmet";
+import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import { connectDatabase } from "./config/mongoose";
 import appRoutes from "./routes";
@@ -6,9 +8,11 @@ import swaggerDoc from "./docs.json";
 
 const app: Application = express();
 
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc, { customSiteTitle: "Orange Brix API" }));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc, { customSiteTitle: "Orange Brix Test API" }));
 
 app.get("/", (req: Request, res: Response) => {
   res.redirect("/docs");
